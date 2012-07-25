d0831276182244498f2bebd1a10184d8a8977848
Jerry Lundström <lundstrom.jerry@gmail.com>
2012-07-25 09:56:46 +0200

    The command callback was holding refs so we release it when done to let everything go.

--- lib/Lim/Plugin/SoftHSM/Server.pm
+++ lib/Lim/Plugin/SoftHSM/Server.pm
@@ -442,6 +442,7 @@ sub CreateInitToken {
             }
             else {
                 $self->Successful($cb);
+                undef($cmd_cb);
             }
         };
         $cmd_cb->();
